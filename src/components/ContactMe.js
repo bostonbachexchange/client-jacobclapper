77311d85cd403a2462125a7a9d1ae4384e8e1baa
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-01-18 22:25:00 -0500

    updated contact emailjs

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,61 +1,55 @@
+import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
-
-const formStyle = {
-    padding: '20px',
-    margin: '0 auto',
-    width: '80%',
- }
- 
- const inputTextarea = {
-    size: '18px',
-    padding: '10px',
-    marginBottom: '20px',
-    width: '100%',
-    boxSizing: 'border-box',
- }
- 
-const ContactMe = () => {
-
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
- 
-    const handleNameChange = (e) => {
-       setName(e.target.value);
-    }
- 
-    const handleEmailChange = (e) => {
-       setEmail(e.target.value);
-    }
- 
-    const handleMessageChange = (e) => {
-       setMessage(e.target.value);
-    }
- 
-    const sendEmail = () => {
-       emailjs.send("YOUR_EMAIL_SERVICE_ID", "YOUR_TEMPLATE_ID", {name, email, message});
-    }
 
 
-    return (
-        <> 
-            <div className="m-3">
-                <h1 className="text-center ls-3 mt-4">Contact</h1>
-                <p className="m-3">Thank you for visiting my music website! I am a professional piano player and instructor, and I am available for both piano lessons and gigs. Whether you're a beginner looking to learn the basics or an experienced musician looking to take your skills to the next level, I am here to help. If you're interested in scheduling a lesson or booking me for an event, please don't hesitate to contact me. I will respond to your message as soon as possible. Thank you again for your interest in my services!</p>
-            </div>
-            <form style={formStyle}>
-                <input type="text" required value={name} onChange={handleNameChange} placeholder="Your name" style={inputTextarea}/>
-                <input type="email" required value={email} onChange={handleEmailChange} placeholder="Your email" style={inputTextarea}/>
-                <textarea value={message} required onChange={handleMessageChange} placeholder="Your message" style={inputTextarea}></textarea>
-                <Button type="submit" onClick={sendEmail}>Send</Button>
+export const ContactMe = () => {
+
+    const formStyle = {
+        padding: '20px',
+        margin: '0 auto',
+        width: '80%',
+    }
+ 
+    const inputTextarea = {
+        size: '18px',
+        padding: '10px',
+        marginBottom: '20px',
+        width: '100%',
+        boxSizing: 'border-box',
+    }
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_6ua4q8w", "template_jpg5dtl",  form.current, '38n3G7bbp-a_O5PNa')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <div className='mb-0 pb-2' style={{backgroundColor: 'lightblue'}}>
+        <div className='p-3' style={{opacity: '1.0'}}>
+            <div className="">
+				<h2 className="m-auto p-1 text-center rounded-pill border-top border-bottom" style={{width: '200px', boxShadow: '1px 1px 1px black', backgroundColor: 'white'}}>Contact</h2>
+			</div>
+            <p className="m-3">Thank you for visiting my music website! I am a professional piano player and instructor, and I am available for both piano lessons and gigs. Whether you're a beginner looking to learn the basics or an experienced musician looking to take your skills to the next level, I am here to help. If you're interested in scheduling a lesson or booking me for an event, please don't hesitate to contact me. I will respond to your message as soon as possible. Thank you again for your interest in my services!</p>
+        </div>
+            <form ref={form} style={formStyle} onSubmit={sendEmail}>
+            {/* <label>Name</label> */}
+            <input type="text" required placeholder="Your name" style={inputTextarea} name="name" />
+            {/* <label>Email</label> */}
+            <input type="email" required style={inputTextarea} placeholder="Your email" name="email" />
+            {/* <label>Message</label> */}
+            <textarea name="message" required placeholder="Your email" style={inputTextarea}/>
+            <input type="submit" value="Send" />
             </form>
+    </div>
+  );
+};
 
-
-        </>
-
-    )
-}
 
 export default ContactMe
--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,6 @@ const divStyle = {
 		<>
 			<div style={divStyle} className=''>
 			<div className='p-3' style={{opacity: '1.0'}}>
-			<i class="fa fa-google" aria-hidden="true" position='absolute'></i>
 				<h2 className="m-auto p-1 text-center rounded-pill border-top border-bottom" style={{width: '200px', boxShadow: '1px 1px 1px black', backgroundColor: 'white'}}>Biography</h2>
 			</div>
 			<div class='bioPage'>
@@ -23,7 +22,20 @@ const divStyle = {
 					<img className='m-4 mt-5' src={profilepic} style={{width: '95%', border: '2px solid white', borderRadius: '10px'}}></img>
 				</div>
 				<div style={{flex: '6', color: 'white', backgroundColor: ''}}>
-					<p className="m-4 mt-5 fs-5" style={{}}>Jacob Clapper current serves as music director for the Unitarian Universalist All Souls Church in Braintree, where he has worked since 2015. Jacob Clapper has been a Collaborative Pianist pianist for Curry College from 2016 to 2020, and he taught Private Music Instruction courses at the college from 2018 to 2020. He was also a collaborative pianist for Duxbury Public Schools from 2014 until 2019. He performed with Duxbury High School Chamber Singers in concerts at Strathmore Hall, Carnegie Hall and Disney. 
+					<p className="m-4 mt-5 fs-5" style={{}}>
+					Jacob Clapper is a highly accomplished pianist and music instructor, who has had a successful career in various roles in the music industry. He currently serves as the Music Director for the Unitarian Universalist All Souls Church in Braintree, where he has been working since 2015. He has also worked as a Collaborative Pianist for Curry College from 2016 to 2020 and taught private music instruction courses there from 2018 to 2020.
+					<br></br>
+					<br></br>
+					He has an extensive experience as a collaborative pianist, having worked with Duxbury Public Schools from 2014 until 2019, where he performed with the Duxbury High School Chamber Singers in concerts at Strathmore Hall, Carnegie Hall, and Disney. He currently teaches piano lessons in Duxbury, Braintree, and Melrose.
+					<br></br>
+					<br></br>
+					Jacob Clapper received his Bachelor of Arts from Texas A&M University-Commerce in 2011 and was awarded a Graduate Assistantship at the University of Florida. During his time as a Graduate Assistant, he taught undergraduate courses in piano skills and accompanied the Men’s Glee Club. He graduated from the University of Florida with a Master’s of Music degree and a concentration in piano performance in 2013.
+					<br></br>
+					<br></br>
+					Throughout his career, Jacob has received piano instruction from distinguished teachers and artists such as Luis Sanchez and Kevin Orr. He has also participated in the University of Florida International Piano Festival and performed in an Honors Recital at Steinway Hall in Plano, Texas.
+					<br></br>
+					<br></br>	
+					{/* Jacob Clapper current serves as music director for the Unitarian Universalist All Souls Church in Braintree, where he has worked since 2015. Jacob Clapper has been a Collaborative Pianist pianist for Curry College from 2016 to 2020, and he taught Private Music Instruction courses at the college from 2018 to 2020. He was also a collaborative pianist for Duxbury Public Schools from 2014 until 2019. He performed with Duxbury High School Chamber Singers in concerts at Strathmore Hall, Carnegie Hall and Disney. 
 					He teaches piano lessons in Duxbury, Braintree, and Melrose. 
 					<br></br>
 					<br></br>
@@ -31,7 +43,7 @@ const divStyle = {
 					<br></br>
 					<br></br>
 					Jacob has received piano instruction from distinguished teachers and artists such as Luis Sanchez, and Kevin Orr. He has also participated in the University of Florida International Piano Festival, and performed in an Honors Recital at Steinway Hall in Plano, Texas. 
-					<br></br>
+					<br></br> */}
 					</p>
 
 				</div>
