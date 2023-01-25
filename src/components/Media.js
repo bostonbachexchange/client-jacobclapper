dbb7b0a7c52f6b1dee371661615cf13463baa540
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-01-24 23:23:39 -0500

    .

--- src/components/Media.js
+++ src/components/Media.js
@@ -3,9 +3,16 @@ import AudioPlayer from "./AudioPlayer"
 import MediaVideos from "./MediaVideos/MediaVideos";
 // import track1 from '../assets/recordings/Footprints-in-the-Snow.mp3';
 // import track2 from '../assets/recordings/For-the-Flowers.mp3';
-import track3 from '../assets/recordings/Freundliche-Landschaft.mp3';
-import track4 from '../assets/recordings/Jobim-MP3.mp3';
-import track5 from '../assets/recordings/Abschied.mp3';
+import track1 from '../assets/recordings/Eintritt.mp3';
+import track2 from '../assets/recordings/Jager-auf-def-Lauer.mp3';
+import track3 from '../assets/recordings/Einsame-Blumen.mp3';
+import track4 from '../assets/recordings/Verrufene-Stelle.mp3';
+import track5 from '../assets/recordings/Freundliche-Landschaft.mp3';
+import track6 from '../assets/recordings/Herberge.mp3';
+import track7 from '../assets/recordings/Vogel-als-Prophet.mp3';
+import track8 from '../assets/recordings/Jagdlied.mp3';
+import track9 from '../assets/recordings/Abschied.mp3';
+import track10 from '../assets/recordings/Jobim-MP3.mp3';
 // import track6 from '../assets/recordings/Blue-in-Green.mp3';
 // import track7 from '../assets/recordings/christmas-time.mp3';
 // import track8 from '../assets/recordings/Consolation.mp3';
@@ -15,7 +22,6 @@ import track5 from '../assets/recordings/Abschied.mp3';
 // import track12 from '../assets/recordings/Gnossienne-No-2.mp3';
 // import track13 from '../assets/recordings/Gnossienne-No-3.mp3';
 // import track14 from '../assets/recordings/Greensleeves.mp3';
-import track15 from '../assets/recordings/Herberge_1.mp3';
 // import track16 from '../assets/recordings/Lonnies-Lament.mp3';
 // import track17 from '../assets/recordings/My-Favorite-Things.mp3';
 // import track18 from '../assets/recordings/scarlatti-a-minor.mp3';
@@ -24,9 +30,15 @@ import track15 from '../assets/recordings/Herberge_1.mp3';
 // import track21 from '../assets/recordings/Traumerei.mp3';
 
 const album1 = [
-  { title: 'Abschied', url: track5, duration: null, composer: 'Robert Schuman', performer: 'Jacob Clapper' },
-  { title: 'Freundliche Landschaft', url: track3, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
-  { title: 'Herberge', url: track15, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Eintritt', url: track1, duration: null, composer: 'Robert Schuman', performer: 'Jacob Clapper' },
+  { title: 'Jager auf der Lauer', url: track2, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Einsame Blumen', url: track3, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Verrufene Stelle', url: track4, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Freundliche Landschaft', url: track5, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Herberge', url: track6, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Vogel als Prophet', url: track7, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Jagdlied', url: track8, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
+  { title: 'Abschied', url: track9, duration: null, composer: 'Robert Schumann', performer: 'Jacob Clapper' },
 
 //   { title: 'Track 2', url: track2, duration: null, composer: 'John Smith', performer: 'Jane Smith' },
 ];
@@ -68,7 +80,18 @@ const Media = () => {
 			<div className='mediaPage mediaReverse' >
 					<div className='discriptionWeb'>
 						<h3><strong style={yujaColor}>Schumann</strong> | <em>Waldszenen Opus 82</em> </h3>
-						<p className="m-3 fs-5">"Forest Scenes"
+						<p className="m-3 fs-5">Waldszenen Opus 82, also known as "Forest Scenes," is a set of nine piano pieces composed by Robert Schumann in 1848-1849. The pieces are meant to evoke the feeling of being in a forest, with each piece having its own unique character and atmosphere.
+						<ol style={{textAlign: 'left', fontSize: '0.7em'}} className='m-4'>
+							<li >Eintritt (Entrance)</li>
+							<li>Jäger auf der Lauer (Hunter on the Lookout)</li>
+							<li>Einsame Blumen (Lonely Flowers)</li>
+							<li>Verrufene Stelle (Haunted Place)</li>
+							<li>Freundliche Landschaft (Friendly Landscape)</li>
+							<li>Herberge (Wayside Inn)</li>
+							<li>Vogel als Prophet (Bird as Prophet)</li>
+							<li>Jagdlied (Hunting Song)</li>
+							<li>Abschied (Farewell)</li>
+						</ol>
 						</p>
 					</div>
 					<div className='text-center' style={{flex: '6'}}>
